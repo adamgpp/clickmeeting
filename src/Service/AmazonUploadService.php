902633060e125ea0ddc154e58631cb8b19987cbb
Adam <adamdawidgula@gmail.com>
2020-07-13 22:31:53 +0200

    use Leage Flysystem Amazon adapter

--- src/Service/AmazonUploadService.php
+++ src/Service/AmazonUploadService.php
@@ -5,21 +5,35 @@ declare(strict_types=1);
 namespace App\Service;
 
 use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
 
 final class AmazonUploadService implements UploadServiceInterface
 {
     private $bucket;
-    private $args;
+    private $credentials;
+    private $region;
+    private $version;
+    private $amazon_uploads_directory;
 
-    public function __construct(string $bucket, array $args)
+    public function __construct(string $bucket, array $credentials, string $region, string $version, string $amazon_uploads_directory)
     {
         $this->bucket = $bucket;
-        $this->args = $args;
+        $this->credentials = $credentials;
+        $this->region = $region;
+        $this->version = $version;
+        $this->amazon_uploads_directory = $amazon_uploads_directory;
     }
 
     public function upload(\SplFileInfo $file): void
     {
-        $client = new S3Client($this->args);
-        $client->upload($this->bucket, $file->getPathname(), file_get_contents($file->getPathname()));
+        $client = new S3Client([
+            'credentials' => $this->credentials,
+            'region' => $this->region,
+            'version' => $this->version,
+        ]);
+        $adapter = new AwsS3Adapter($client, $this->bucket);
+        $filesystem = new Filesystem($adapter);
+        $filesystem->write($this->amazon_uploads_directory . '/' . $file->getFilename(), file_get_contents($file->getPathname()));
     }
 }
